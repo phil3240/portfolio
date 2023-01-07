cd0d950edc9bd2fe2d48ff1f7445ce1fd6ee8d8f
Phillip Kongstad <phil3240@stud.kea.dk>
2023-01-07 23:42:52 +0100

    Start på java og cv

---
+++ portfolio/scroll.js
@@ -0,0 +1,11 @@
+const tema = document.querySelectorAll("section");
+
+window.addEventListener("scroll", nyttema);
+
+function nyttema() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+}
+
+tema.forEach((section) => {
+  const temaTop = tema;
+});
