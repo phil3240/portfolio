f3a32e3be649a357536a8bb9b0fcaa52dc5bdba4
Phillip Kongstad <phil3240@stud.kea.dk>
2023-01-08 16:42:28 +0100

    tekster og billeder til opgaverne er på

--- portfolio/scroll.js
+++ portfolio/scroll.js
@@ -3,6 +3,7 @@ const tema = document.querySelectorAll("section");
 window.addEventListener("scroll", nyttema);
 
 function nyttema() {
+  console.log("du har scrollet");
   const triggerBottom = (window.innerHeight / 5) * 4;
 }
 
